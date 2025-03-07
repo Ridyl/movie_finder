cc449e2dea8c763106b984dac0220e8f4d6d068f
Ridyl <joey.dalatri214@gmail.com>
2025-03-07 17:54:55 -0500

    Finished top10 card component that creates cards based off of data given. Card dynamic opens using tailwind animations.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,11 +5,21 @@ import { Input } from './ui/input';
 import TopTen from './TopTen';
 
 export default function Home() {
+	// Need to create components that will replace the title, when chosen the current homepage choice will change.
+	// All will be replaced when search is done by user.
+	function DesktopMenu() {
+		// This will be for the desktop PopOver component
+	}
+
+	function MobileMenu() {
+		// This will be for the mobile Drawer component
+	}
+
 	return (
 		<>
 			<div className='flex flex-col w-full text-center'>
 				<h1 className='text-2xl font-semibold mb-4 underline underline-offset-5'>
-					IMDb Media Finder
+					Media Finder
 				</h1>
 				<Input className='mb-2' type='text' placeholder='Search...' />
 				<div className='flex justify-center'>
@@ -20,9 +30,11 @@ export default function Home() {
 				</div>
 			</div>
 			<div className='mt-5'>
-				<h1 className='text-lg font-semibold mb-4 underline underline-offset-5'>
-					IMDb Top 10
-				</h1>
+				{/* Going to replace this with a schadcn ComboBox component that will be responsive for mobile and desktop use. */}
+				{/* Will have 3 options, IMDb top 10, Top Grossing, and Top Marvel. */}
+				<div className='flex justify-center'>
+					<h1 className='text-lg font-semibold mb-4'>IMDb Top 10</h1>
+				</div>
 				<TopTen />
 			</div>
 		</>
